58df1c0ef919ddef0bf78dce31b9028173efa151
Eliel <elielmichel@gmail.com>
2022-09-03 16:01:23 -0300

    feat: implement seconds into clock

--- src/Clock.java
+++ src/Clock.java
@@ -1,26 +1,34 @@
 public class Clock {
     NumberDisplay hours;
     NumberDisplay minutes;
+    NumberDisplay seconds;
     String displayString;
 
     public Clock() {
         hours = new NumberDisplay(24);
         minutes = new NumberDisplay(60);
+        seconds = new NumberDisplay(60);
         updateDisplay();
     }
 
-    public void setTime(int hour, int minute) {
+    public void setTime(int hour, int minute, int second) {
         hours.setValue(hour);
         minutes.setValue(minute);
+        seconds.setValue(second);
         updateDisplay();
     }
 
     private void updateDisplay() {
-        displayString = hours.getDisplayString() + ":" + minutes.getDisplayString();
+        displayString = hours.getDisplayString() + ":" + minutes.getDisplayString() + ":" + seconds.getDisplayString();
     }
 
     public void timeTick() {
-        minutes.increment();
-        if (minutes.getValue() == 0) hours.increment();
+        seconds.increment();
+        if (seconds.getValue() == 0) minutes.increment();
+        if (minutes.getValue() == 0 && seconds.getValue() == 0) hours.increment();
+
+        updateDisplay();
+
+        System.out.println(displayString);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         final Clock clock = new Clock();
+
+        clock.setTime(15, 59, 57);
+
+        clock.timeTick();
+        clock.timeTick();
+        clock.timeTick();
+        clock.timeTick();
+        clock.timeTick();
     }
 }
